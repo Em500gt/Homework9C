210625725fc64e65ae1d18bf12de3e01e716aa3b
Egor Malinovski <em500gt@gmail.com>
2022-10-16 23:15:09 +0300

    Home work 9

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using static System.Console;
+Clear();
+
+Write("Задайте значения M и N: ");
+string[] parameter = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+WriteLine(Summ(int.Parse(parameter[0]), int.Parse(parameter[1]), 0));
+
+int Summ(int m, int n, int sum)
+{
+    return m >= n + 1? sum: Summ(m + 1, n, sum + m);
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+using static System.Console;
+Clear();
+Write("Введите два неотрицательных числа m и n: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int a = int.Parse(parameters[0]);
+int b = int.Parse(parameters[1]);
+if(a < 0 || b < 0) return;
+WriteLine(Aker(a,b));
+
+int Aker(int m, int n)
+{
+    return m == 0? n + 1: (m > 0 && n == 0)? Aker(m - 1, 1): Aker(m - 1, Aker(m, n - 1));
+}
